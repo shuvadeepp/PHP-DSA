97582f06cc1bdfb88b611ad870f56ee970491b7f
Shuvadeep Podder <shuvadeep.podder@corp.csmpl.com>
2024-12-21 21:49:52 +0530

    practice array_map

--- unique_array_sum.php
+++ unique_array_sum.php
@@ -2,32 +2,51 @@
 
 /* using pre-defind function : */
 
-$arr = [1,3,2,3,2,4];
-$unique=array_unique($arr);
-$toSum=array_sum($unique);
-print_r($toSum);
+// $arr = [1,3,2,3,2,4];
+// $unique=array_unique($arr);
+// $toSum=array_sum($unique);
+// print_r($toSum);
 
 /* using array_map pre-defind function : */
 
-$arr = [1,3,2,3,2,4];
-$unique=array_unique($arr); 
-$total=0;
-array_map(function($num) use (&$total){
-    return $total+=$num;
-}, $unique);
+// $arr = [1,3,2,3,2,4];
+// $unique=array_unique($arr); 
+// $total=0;
+// array_map(function($num) use (&$total){
+//     return $total+=$num;
+// }, $unique);
  
-print_r($total);
+// print_r($total);
+
+// another way to using array_map function
+
+$arr = [1,3,2,3,2,4]; 
+function get_unique($param){
+    static $cal=0; 
+    $cal+=$param;
+    return $cal;
+}
+ 
+$unique=[];
+foreach($arr as $val){
+    if(!in_array($val, $unique)){
+        $unique[]=$val; 
+    }
+} 
+array_map("get_unique", $unique);
+
+// print_r(get_unique(0));
 
 /* without using pre-defind function : */
 
-$arr = [1,3,2,3,2,4];
-$unique=[];
-$total=0;
+// $arr = [1,3,2,3,2,4];
+// $unique=[];
+// $total=0;
 
-foreach($arr as $sum){
-  if(!in_array($sum, $unique)){
-    $unique[]=$sum;
-    $total+=$sum;
-  }
-}
-echo $total;
+// foreach($arr as $sum){
+//   if(!in_array($sum, $unique)){
+//     $unique[]=$sum;
+//     $total+=$sum;
+//   }
+// }
+// echo $total;
